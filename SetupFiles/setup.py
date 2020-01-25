74e803bd7516c6602d9a1077d40a1cb36bd55519
Elisabeth Reid <16.elisabethw@gmail.com>
2020-01-24 19:35:28 -0500

    Create setup.py
    setup.py file to install ccmsuite in python site-packages. Usage directions in Installation wiki documentation.

---
+++ SetupFiles/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="ccmsuite",
+    version=0.2,
+    author="Terry Stewart, Sterling Somers",
+    author_email="",
+    packages=[],
+    scripts=[],
+    url="https://github.com/sterlingsomers/pythonACTR3",
+    description = "Python Cognitive Modelling Suite Python ACT-R"
+)
